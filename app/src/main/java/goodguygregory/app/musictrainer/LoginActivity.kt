28839cbe7944acd0cec8fb9b34eb31db693e963f
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-10 08:33:39 -0500

    adds Signup and Login Activities on start

--- app/src/main/java/goodguygregory/app/musictrainer/LoginActivity.kt
+++ app/src/main/java/goodguygregory/app/musictrainer/LoginActivity.kt
@@ -1,12 +1,41 @@
 package goodguygregory.app.musictrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+//    Creates Variables for the activity:
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var signUpText: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginEmail = findViewById(R.id.login_email)
+        loginPassword = findViewById(R.id.login_password)
+        loginButton = findViewById(R.id.action_sign_in)
+        signUpText = findViewById(R.id.not_user_register)
+
+
+        loginButton.setOnClickListener{
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+
+        }
+
+        signUpText.setOnClickListener {
+//            Start Signup Activity:
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/goodguygregory/app/musictrainer/SignupActivity.kt
+++ app/src/main/java/goodguygregory/app/musictrainer/SignupActivity.kt
@@ -1,12 +1,37 @@
 package goodguygregory.app.musictrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 
 class SignupActivity : AppCompatActivity() {
+
+//    Adds variables for signup
+    private lateinit var userEmail: EditText
+    private lateinit var userPassword: EditText
+    private lateinit var signUp: Button
+    private lateinit var loginSwitch: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+//        login Creds:
+        userEmail = findViewById(R.id.prompt_email)
+        userPassword = findViewById(R.id.prompt_password)
+
+//      Buttons:
+        signUp = findViewById(R.id.action_signup)
+        loginSwitch = findViewById(R.id.already_user_login)
+
+        loginSwitch.setOnClickListener {
+//            Start LoginActivity:
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
